abbaa4ad2ad610ae5336929680bf9bf507d5906d
Navya Madiraju <navyamadiraju@Navyas-MacBook-Pro.local>
2025-07-04 00:30:04 +0530

    Fix deploy script and homepage for GitHub Pages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = "https://optirelief-backend.onrender.com";
+
 
 export const apiService = axios.create({
   baseURL: API_BASE_URL,
